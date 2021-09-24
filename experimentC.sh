4b60d85ee5336a6153d10ba6ba503b3fa0d5fba0
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-09-24 11:14:01 +0200

    Add files via upload

---
+++ experimentC.sh
@@ -0,0 +1,19 @@
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game0.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result0
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game1.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result1
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game2.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result2
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game3.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result3
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game4.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result4
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game5.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result5
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game6.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result6
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game7.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result7
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game8.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result8
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2021/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2021/models/C1-2-1-1-2/game9.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/C/result9
