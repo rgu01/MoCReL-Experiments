a4fecbe8a0f6af5a76e9e6aa4fd70840ad93a797
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-09-26 13:13:53 +0200

    Update experimentD.sh

--- experimentD.sh
+++ experimentD.sh
@@ -13,7 +13,7 @@ UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so 
 #
 UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/D1-2-1-1-2-nouncertainty/game6.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/D/result6
 #
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/D1-2-1-1-2-nouncertainty/game7.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/D/result7
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/D1-2-1-1-2-nouncertainty/game7.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 5000 --total-runs 5000 --reset-no-better 10 --max-reset-learning 3 >> results/D/result7
 #
 UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/D1-2-1-1-2-nouncertainty/game8.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 500 --total-runs 500 --reset-no-better 10 --max-reset-learning 3 >> results/D/result8
 #
