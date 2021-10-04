4a0da998ef5092e522fc27228785014810702781
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-10-04 20:40:12 +0200

    Add files via upload

--- experimentR.sh
+++ experimentR.sh
@@ -1,11 +1,7 @@
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-10.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-10
-#
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-9.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-9
-#
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-8.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-8
-#
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-7.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-7
-#
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-6.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-6
-#
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-5.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100 --total-runs 100 --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-5
+for i in 100 200 300 400 500
+do
+for j in 10 9 8 7 6 5
+do
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-"${j}".xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs "${i}" --total-runs "${i}" --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-"${j}"
+done
+done
