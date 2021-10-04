829d01ee47e9ab82ab0ea877595937eea6eb80cf
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-10-04 20:11:53 +0200

    Add files via upload

---
+++ experimentRuns.sh
@@ -0,0 +1,14 @@
+for i in 1 2 3 4 5
+do
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-10.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-10
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-9.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-9
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-8.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-8
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-7.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-7
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-6.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-6
+#
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/Rong/Gu-TACAS2022/models/Runs/game6B-5.xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs 100*i --total-runs 100*i --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-5
+done
