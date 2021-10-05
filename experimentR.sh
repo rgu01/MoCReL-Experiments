37840e764c45e0c7b0b1ec6796595fc06fea4434
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-10-05 09:24:31 +0200

    Update experimentR.sh

--- experimentR.sh
+++ experimentR.sh
@@ -1,7 +1,10 @@
-for i in 100 200 300 400 500
+for times in 1 2 3 4 5 6 7 8 9 10
 do
-for j in 10 9 8 7 6 5
+for runs in 100 200 300 400 500
 do
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-"${j}".xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs "${i}" --total-runs "${i}" --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-"${j}"
+for files in 8 7
+do
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/Gu-TACAS2022/lib/libexternal_learning.so ./stratego/verifyta -u ~/uppaal/Gu-TACAS2022/models/Runs/game6B-"${files}".xml -D 0.01 --learning-method -1 -s --max-iterations 30 --eval-runs 100 --good-runs "${runs}" --total-runs "${runs}" --reset-no-better 10 --max-reset-learning 3 >> results/Runs/result6B-"${files}"-"${times}"
+done
 done
 done
